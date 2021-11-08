6ed1858c771408616fc8c542edf1d6ce4bf1d09c
moscowsky <moscowskyad@gmail.com>
2021-11-08 03:21:45 +0300

    eod compiles, tested buttons for file and base loading

--- gui_eod.cpp
+++ gui_eod.cpp
@@ -1,6 +1,8 @@
 #include "gui_eod.h"
 #include "./ui_gui_eod.h"
 #include <QSyntaxHighlighter>
+#include <QFileDialog>
+#include <QMessageBox>
 
 gui_eod::gui_eod(QWidget *parent)
     : QMainWindow(parent)
@@ -12,5 +14,34 @@ gui_eod::gui_eod(QWidget *parent)
 gui_eod::~gui_eod()
 {
     delete ui;
+}
+
+
+void gui_eod::on_pb_openImage_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Open Image"), "/home", tr("Image Files (*.png *.jpg *.bmp)"));
+    QPixmap pm(fileName);
+    ui->l_image->setPixmap(pm);
+    ui->l_image->setScaledContents(true);
+
+}
+
+
+void gui_eod::on_pb_openBase_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Open Object Base"), "/home", tr("XML Files (*.xml)"));
+
+    objectBase = new eod::ObjectBase();
+    objectBase->loadFromXML(fileName.toStdString());
+
+    QFile base(fileName);
+    if(!base.open(QIODevice::ReadOnly)) {
+        QMessageBox::information(0, "error", base.errorString());
+    }
+    QTextStream in(&base);
+    ui->te_ob_editor->setText(in.readAll());
+
 }
 
--- gui_eod.h
+++ gui_eod.h
@@ -17,7 +17,14 @@ public:
     gui_eod(QWidget *parent = nullptr);
     ~gui_eod();
 
+private slots:
+    void on_pb_openImage_clicked();
+
+    void on_pb_openBase_clicked();
+
 private:
     Ui::gui_eod *ui;
+
+    eod::ObjectBase * objectBase;
 };
 #endif // GUI_EOD_H
